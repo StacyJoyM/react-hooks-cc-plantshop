7bbff02801091ae65083ec587cd8b587c488e039
Stacy-JoyM <jstacymuthoka@gmail.com>
2024-11-05 02:31:59 +0300

    plantsy challenge

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React , {useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import plantsArr from "../db.json"
 
 function PlantPage() {
+  const [plants, setPlants ] = useState(plantsArr)
+  const [search, setSearch] = useState("")
+
+  function onSubmitPlant(newPlant){
+    setPlants((prevPlants)=> [...prevPlants, newPlant])
+  }
+
+  function handleSearch(event){
+    setSearch(event.target.value)
+  }
+
+  const plantsToDisplay= plants.filter((plant)=>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm 
+      onSubmitPlant={onSubmitPlant}/>
+      <Search search={search} handleSearch={handleSearch}/>
+      <PlantList plants={plantsToDisplay}/>
     </main>
   );
 }
